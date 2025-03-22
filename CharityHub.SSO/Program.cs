be0a355bcf0d157dbf2d43a053972c2a96eaaf61
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-03-22 13:08:32 +0330

    add serilog to project

--- CharityHub.SSO/Program.cs
+++ CharityHub.SSO/Program.cs
@@ -1,57 +1,32 @@
 ﻿using CharityHub.SSO;
 using CharityHub.SSO.Configs;
 using Serilog;
-using System;
-
-Log.Logger = new LoggerConfiguration()
-    // .WriteTo.Console()
-    .CreateBootstrapLogger();
 
 Console.WriteLine("Starting up");
 
-try
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(ctx.Configuration));
+
+var appSettings = new AppSettings();
+builder.Configuration.Bind(appSettings);
+
+builder.Services.AddSingleton(appSettings);
+
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
+
+if (args.Contains("/seed"))
 {
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Host.UseSerilog((ctx, lc) => lc
-        // .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-        .Enrich.FromLogContext()
-        .ReadFrom.Configuration(ctx.Configuration));
-
-    //builder.WebHost.ConfigureKestrel(serverOptions =>
-    //{
-    //    serverOptions.ListenAnyIP(5001); // Listen on all IPs on port 5000
-    //});
-
-    // Bind configuration to AppSettings model
-    var appSettings = new AppSettings();
-    builder.Configuration.Bind(appSettings);
-
-    // Add AppSettings to the DI container
-    builder.Services.AddSingleton(appSettings);
-
-    var app = builder
-        .ConfigureServices()
-        .ConfigurePipeline();
-
-    // this seeding is only for the template to bootstrap the DB and users.
-    // in production you will likely want a different approach.
-    if (args.Contains("/seed"))
-    {
-        Console.WriteLine("Seeding database...");
-        await SeedData.EnsureSeedDataAsync(app);
-        Console.WriteLine("Done seeding database. Exiting.");
-        return;
-    }
-
-    app.Run();
+    Console.WriteLine("Seeding database...");
+    await SeedData.EnsureSeedDataAsync(app);
+    Console.WriteLine("Done seeding database. Exiting.");
+    return;
 }
-catch (Exception ex) when (ex is not HostAbortedException)
-{
-    Console.WriteLine(ex.Message);
-}
-finally
-{
-    Console.WriteLine("Shut down complete");
-    // Log.CloseAndFlush();
-}
+
+app.Run();
+
+Console.WriteLine("Shut down complete");
